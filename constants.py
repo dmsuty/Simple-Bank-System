d849e9684d40b76232b78f768475620dbf2d6c70
dmsuty <dmsutyy@gmail.com>
2022-05-02 19:21:26 +0300

    added constants file

---
+++ constants.py
@@ -0,0 +1,4 @@
+MaxKazachestvoVerifiedCredit = 2_500_000_000
+MaxKazachestvoNotVerifiedCredit = 100_000_000
+MaxKazachestvoVerifiedDeposit = 10_000_000_000
+MaxKazachestvoNotVerifiedDeposit = 2_000_000_000
