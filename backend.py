0f0b6b226d33ef833c5cf3fc156036335a3197b5
dmsuty <dmsutyy@gmail.com>
2022-05-09 04:06:48 +0300

    checkpoint1

--- backend.py
+++ backend.py
@@ -4,10 +4,15 @@ from db import BankDataBase
 import constants
 
 
+def str_to_date(strdate):
+    y, m, h = map(int, strdate.split(':'))
+    return timedelta(y, m, h)
+
+
 "======================= Clients =============================================="
 
 
-class Client:
+class Client:   # make Builder later TODO
     def __init__(self, params):
         self.first_name = params[0]
         self.second_name = params[1]
@@ -16,23 +21,42 @@ class Client:
         self.passport_number = params[4]
         self.phone_number = params[5]
         self.password = params[6]
+        self.write_to_bd()
+
+    def change_phone_number(self):  # TODO
+        pass
+
+    def change_password(self):  # TODO
+        pass
+
+    def change_address(self, new_address):  # TODO
+        pass
+
+    def add_passport(self):    # TODO
+        if self.passport_number == '':
+            raise Exception
+        pass
 
     def is_verified(self):
         return self.address and self.passport_series and self.passport_number
 
-    def params(self):
+    def bd_params(self):
         return (self.first_name, self.second_name, self.address,
                 self.passport_series, self.passport_number, self.phone_number, self.password)
 
     def write_to_bd(self):
-        if bank_data_base.get_client_params(self.phone_number) is not None:
-            bank_data_base.add_new_client(self.params())
+        if bank_data_base.get_client_params(self.phone_number) is None:
+            bank_data_base.add_new_client(self.bd_params())
+
+    @staticmethod
+    def get_client(phone_number):
+        return Client(bank_data_base.get_client_params(phone_number))
 
 
 "======================== there are accounts =================================="
 
 
-class AbstractAccount(ABC):
+class Account(ABC):
     @abstractmethod
     def __init__(self):
         pass
@@ -49,33 +73,48 @@ class AbstractAccount(ABC):
     def get_id(self):
         pass
 
+    @staticmethod
+    def get_account(account_id):
+        params = bank_data_base.get_account_params(account_id)
+        type, bank = params[2:4]
+        if type == 'credit' and bank == 'Kazachestvo':
+            return KazachestvoCredit.params_to_account(params)
+        if type == 'debit' and bank == 'Kazachestvo':
+            return KazachestvoDebit.params_to_account(params)
+        if type == 'deposit' and bank == 'Kazachestvo':
+            return KazachestvoDeposit.params_to_account(params)
+        pass    # TODO may be other banks
 
-class Deposit(AbstractAccount):
+
+class Deposit(Account):
     @abstractmethod
     def get_term(self) -> date:
         pass
 
 
-class Credit(AbstractAccount):
+class Credit(Account):
     @abstractmethod
     def get_debt(self) -> int:
         pass
 
 
-class Debit(AbstractAccount):
+class Debit(Account):
     pass
 
 
 class KazachestvoDeposit(Deposit):
-    def __init__(self, client, balance):
-        self.max_deposit = self.max_deposit(client)
-        if balance > self.max_deposit:
-            raise Exception
-        self.balance = balance
-        self.phone_number = client.phone_number
-        self.term = self.new_deposit_term()
-        self.id = bank_data_base.new_account_id()
-        self.write_to_bd()
+    def __init__(self, client=None, balance=None):
+        if client is not None:
+            self.max_deposit = self.max_deposit(client)
+            if balance > self.max_deposit:
+                raise Exception
+            self.balance = balance
+            self.phone_number = client.phone_number
+            self.term = self.new_deposit_term()
+            self.id = bank_data_base.new_account_id()
+            self.type = 'deposit'
+            self.bank = 'Kazachestvo'
+            self.write_to_bd()
 
     def get_term(self):
         return self.term
@@ -97,39 +136,49 @@ class KazachestvoDeposit(Deposit):
         self.balance += deposit_amount
         self.write_to_bd()
 
-    @staticmethod
-    def new_deposit_term():
-        return date.today() + timedelta(days=500)
+    def bd_params(self):
+        return self.id, self.phone_number, self.type, self.bank, self.balance, str(self.term)
 
-    @staticmethod
-    def max_deposit(client):
+    def write_to_bd(self):
+        if bank_data_base.get_account_params(self.id) is None:
+            bank_data_base.add_new_account(self.bd_params())
+        else:
+            bank_data_base.set_balance(self.balance, self.id)
+
+    def max_deposit(self):
+        client = Client.get_client(self.phone_number)
         if client.is_verified():
             return constants.MaxKazachestvoVerifiedDeposit
         return constants.MaxKazachestvoNotVerifiedDeposit
 
-    def params(self):
-        return self.id, self.phone_number, 'deposit', self.balance, str(self.term)
+    @staticmethod
+    def new_deposit_term():
+        return date.today() + timedelta(days=500)
 
-    def write_to_bd(self):
-        if bank_data_base.get_account_params(self.id) is None:
-            bank_data_base.add_new_account(self.params())
-        else:
-            bank_data_base.set_balance(self.balance, self.id)
+
+    @staticmethod
+    def params_to_account(account_params):
+        account = KazachestvoDeposit()
+        account.account_id = account_params[0]
+        account.phone_number = account_params[1]
+        account.type = account_params[2]
+        account.balance = account_params[3]
+        account.term = str_to_date(account_params[4])
+        return account
 
 
 class KazachestvoCredit(Credit):
-    def __init__(self, client):
-        self.balance = 0
-        self.phone_number = client.phone_number
-        self.id = bank_data_base.new_account_id()
-        if client.is_verified():
-            self.max_credit = constants.MaxKazachestvoVerifiedCredit
-        else:
-            self.max_credit = constants.MaxKazachestvoNotVerifiedCredit
-        self.write_to_bd()
+    def __init__(self, client=None):
+        if client is not None:
+            self.balance = 0
+            self.phone_number = client.phone_number
+            self.id = bank_data_base.new_account_id()
+            self.type = 'credit'
+            self.bank = "Kazachestvo"
+            self.write_to_bd()
 
     def withdraw(self, write_off):
-        if -(self.balance - write_off) > self.max_credit:
+        if -(self.balance - write_off) > self.max_credit():
             raise Exception
         if self.balance < write_off:
             pass
@@ -146,23 +195,42 @@ class KazachestvoCredit(Credit):
     def get_debt(self) -> int:
         return max(0, -self.balance)
 
-    def params(self):
-        return self.id, self.phone_number, 'credit', self.balance, str(timedelta())
+    def bd_params(self):
+        return self.id, self.phone_number, self.type, self.bank, self.balance, str(timedelta())
 
     def write_to_bd(self):
         if bank_data_base.get_account_params(self.id) is None:
-            bank_data_base.add_new_account(self.params())
+            bank_data_base.add_new_account(self.bd_params())
         else:
             bank_data_base.set_balance(self.balance, self.id)
 
+    def max_credit(self):
+        client = Client.get_client(self.phone_number)
+        if client.is_verified():
+            return constants.MaxKazachestvoVerifiedCredit
+        return constants.MaxKazachestvoNotVerifiedCredit
+
+    @staticmethod
+    def params_to_account(account_params):
+        account = KazachestvoCredit()
+        account.account_id = account_params[0]
+        account.phone_number = account_params[1]
+        account.type = account_params[2]
+        account.bank = account_params[3]
+        account.balance = account_params[4]
+        account.term = str_to_date(account_params[5])
+        return account
+
 
 class KazachestvoDebit(Debit):
-    def __init__(self, client):
-        self.max_balance = self.debit_max_balance(client)
-        self.balance = 0
-        self.phone_number = client.phone_number
-        self.id = bank_data_base.new_account_id()
-        self.write_to_bd()
+    def __init__(self, client=None):
+        if client is not None:
+            self.balance = 0
+            self.phone_number = client.phone_number
+            self.type = "debit"
+            self.bank = "Kazachestvo"
+            self.account_id = bank_data_base.new_account_id()
+            self.write_to_bd()
 
     def withdraw(self,  write_off):
         if self.balance < write_off:
@@ -171,28 +239,39 @@ class KazachestvoDebit(Debit):
         self.write_to_bd()
 
     def deposit(self, deposit_amount):
-        if self.balance + deposit_amount > self.max_balance:
+        if self.balance + deposit_amount > self.max_balance():
             raise Exception
         self.balance += deposit_amount
         self.write_to_bd()
 
     def get_id(self):
-        return self.id
+        return self.account_id
 
-    @staticmethod
-    def debit_max_balance(client):
+    def bd_params(self):
+        return self.account_id, self.phone_number, self.type, self.bank, self.balance, str(timedelta())
+
+    def write_to_bd(self):
+        if not bank_data_base.account_exists(self.account_id):
+            bank_data_base.add_new_account(self.bd_params())
+        else:
+            bank_data_base.set_balance(self.balance, self.account_id)
+
+    def max_balance(self):
+        client = Client.get_client(self.phone_number)
         if client.is_verified():
             return constants.MaxKazachestvoVerifiedDebitBalance
         return constants.MaxKazachestvoNotVerifiedDebitBalance
 
-    def params(self):
-        return self.id, self.phone_number, 'debit', self.balance, str(timedelta())
-
-    def write_to_bd(self):
-        if not bank_data_base.account_exists(self.id):
-            bank_data_base.add_new_account(self.params())
-        else:
-            bank_data_base.set_balance(self.balance, self.id)
+    @staticmethod
+    def params_to_account(account_params):
+        account = KazachestvoDebit()
+        account.account_id = account_params[0]
+        account.phone_number = account_params[1]
+        account.type = account_params[2]
+        account.bank = account_params[3]
+        account.balance = account_params[4]
+        account.term = str_to_date(account_params[5])
+        return account
 
 
 "======================== there are fabrics ==================================="
@@ -228,14 +307,49 @@ class KazachestvoFactory(AbstractAccountFactory):
 
 "================ Transaction ========================="
 
- # class Transaction:
- #    def __init__(self, first_account, second_account, money):
- #        bank_data_base.add_new_transaction()
+
+class Transaction:
+    def __init__(self, creditor, receiver, money, status='confirmed'):
+        self.transaction_id = bank_data_base.new_transaction_id()
+        self.creditor_id = creditor.account_id
+        self.receiver_id = receiver.account_id
+        self.summ = money
+        self.status = status
+        self.write_to_bd()
+        creditor.withdraw(money)
+        receiver.deposit(money)
+
+    def bd_params(self):
+        return self.transaction_id, self.creditor_id, self.receiver_id, self.summ, self.status
+
+    def write_to_bd(self):
+        if bank_data_base.get_transaction_params(self.transaction_id) is None:
+            bank_data_base.add_new_transaction(self.bd_params())
+
+    @staticmethod
+    def get_creditor(transaction_id):
+        params = bank_data_base.get_transaction_params(transaction_id)
+        return params[1]
+
+    @staticmethod
+    def get_receiver(transaction_id):
+        params = bank_data_base.get_transaction_params(transaction_id)
+        return params[2]
+
+    @staticmethod
+    def get_summ(transaction_id):
+        params = bank_data_base.get_transaction_params(transaction_id)
+        return params[3]
+
+    @staticmethod
+    def cancel(transaction_id):
+        creditor = Account.get_account(Transaction.get_creditor(transaction_id))
+        receiver = Account.get_account(Transaction.get_receiver(transaction_id))
+        Transaction(receiver, creditor, Transaction.get_summ(transaction_id), 'cancelled')
+
+
+# def call_collector(bad_client):
+#     pass - should make it later
 
 
 bank_data_base = BankDataBase()
-me = Client(('Дмитрий', 'Сутый', 'ул.Римского-Корсакова, дом8', 1234, 172702, 896895786008, 'w4uthLuh'))
-me.write_to_bd()
-account = KazachestvoFactory.create_debit(me)
-new_account = KazachestvoFactory.create_deposit(me, 100500)
-account.deposit(10)
--- db.py
+++ db.py
@@ -6,28 +6,30 @@ clients_create_team = """CREATE TABLE IF NOT EXISTS clients (first_name, second_
                        address, passport_series, passport_number,
                        phone_number, password)"""
 
-accounts_create_team = """CREATE TABLE IF NOT EXISTS accounts (id, phone_number,
-                        account_type, balance, term)"""
+accounts_create_team = """CREATE TABLE IF NOT EXISTS accounts (account_id, phone_number,
+                        account_type, bank, balance, term)"""
 
 transactions_create_team = """CREATE TABLE IF NOT EXISTS transactions (transaction_id, 
-                                                            first_id, second_id, money)"""
+                                                            creditor_id, receiver_id, summ, status)"""
 
 select_client_team = """SELECT first_name, second_name, address, passport_series, passport_number,
                       phone_number, password FROM clients WHERE phone_number = ?"""
 
-select_account_team = """SELECT id, phone_number, account_type, balance, term
-                       FROM accounts WHERE id = ?"""
+select_account_team = """SELECT account_id, phone_number, account_type, bank, balance, term
+                       FROM accounts WHERE account_id = ?"""
 
-select_transaction_team = """SELECT transaction_id, first_id, second_id, money, 
+select_transaction_team = """SELECT transaction_id, creditor_id, receiver_id, summ, status 
                              FROM transactions WHERE transaction_id = ?"""
 
 add_to_clients_team = "insert into clients values (?, ?, ?, ?, ?, ?, ?)"
 
-add_to_accounts_team = "insert into accounts values (?, ?, ?, ?, ?)"
+add_to_accounts_team = "insert into accounts values (?, ?, ?, ?, ?, ?)"
 
-add_to_transactions_team = "insert into transactions value (?, ?, ?, ?)"
+add_to_transactions_team = "insert into transactions values (?, ?, ?, ?, ?)"
 
-update_account_balance = "update accounts set balance = ? where id = ?"
+change_account_balance = "update accounts set balance = ? where account_id = ?"
+
+change_transaction_status = "update transactions set status = ? where transaction_id = ?"
 
 
 class BankDataBase:
@@ -39,9 +41,15 @@ class BankDataBase:
         self.cursor.execute(transactions_create_team)
 
     def new_account_id(self):
-        result_id = random.randint(10 ** 16, 10 ** 17 - 1)
+        result_id = self.random_int(12)
         while self.get_account_params(result_id) is not None:
-            result_id = random.randint(10 ** 16, 10 ** 17 - 1)
+            result_id = self.random_int(12)
+        return result_id
+
+    def new_transaction_id(self):
+        result_id = self.random_int(16)
+        while self.get_transaction_params(result_id) is not None:
+            result_id = self.random_int(16)
         return result_id
 
     def add_new_client(self, params):
@@ -50,6 +58,10 @@ class BankDataBase:
 
     def add_new_account(self, params):
         self.cursor.execute(add_to_accounts_team, params)
+        self.connection.commit()
+
+    def add_new_transaction(self, params):
+        self.cursor.execute(add_to_transactions_team, params)
         self.connection.commit()
 
     def get_client_params(self, phone_number):
@@ -62,15 +74,31 @@ class BankDataBase:
         if len(row):
             return row[0]
 
-    def account_exists(self, id):
-        return self.get_account_params(id) is not None
+    def get_transaction_params(self, transaction_id):
+        row = self.cursor.execute(select_transaction_team, (transaction_id, )).fetchall()
+        if len(row):
+            return row[0]
+
+    def account_exists(self, account_id):
+        return self.get_account_params(account_id) is not None
 
     def client_exists(self, phone_number):
         return self.get_client_params(phone_number) is not None
 
-    def set_balance(self, new_balance, id):
-        self.cursor.execute(update_account_balance, (new_balance, id))
+    def transaction_exists(self, transaction_id):
+        return self.get_transaction_params(transaction_id) is not None
+
+    def set_balance(self, new_balance, account_id):
+        self.cursor.execute(change_account_balance, (new_balance, account_id))
+        self.connection.commit()
+
+    def set_transaction_status(self, new_status, transaction_id):
+        self.cursor.execute(change_transaction_status, (new_status, transaction_id))
         self.connection.commit()
 
     def close(self):
         self.connection.close()
+
+    @staticmethod
+    def random_int(count_of_digits):
+        return random.randint(10 ** count_of_digits, 10 ** (count_of_digits + 1) - 1)
